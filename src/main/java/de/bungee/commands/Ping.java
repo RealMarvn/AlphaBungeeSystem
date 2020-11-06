c0c089eb72c7138117ea7ab22208551b0a13a45a
root <KillingSausage@gmail.com>
2020-11-06 22:23:03 +0100

    + gradle.build update

--- src/main/java/de/bungee/commands/Ping.java
+++ src/main/java/de/bungee/commands/Ping.java
@@ -2,6 +2,7 @@ package de.bungee.commands;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
