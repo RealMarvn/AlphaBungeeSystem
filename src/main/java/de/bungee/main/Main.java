c92099a9781749bf57597323468c4ee6b173ce34
root <KillingSausage@gmail.com>
2020-11-06 21:53:16 +0100

    On Enable erstellt

--- src/main/java/de/bungee/main/Main.java
+++ src/main/java/de/bungee/main/Main.java
@@ -3,4 +3,15 @@ package de.bungee.main;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        System.out.println("AlphaBungeeSystem online");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
