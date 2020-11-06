c18f43cb1e388faf87abcfc4861c13780706422d
root <KillingSausage@gmail.com>
2020-11-06 23:04:57 +0100

    + ping command

--- src/main/java/de/bungee/commands/Ping.java
+++ src/main/java/de/bungee/commands/Ping.java
@@ -2,14 +2,13 @@ package de.bungee.commands;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class Ping extends Command {
 
     public Ping() {
-     super("getip");
+     super("ping");
     }
 
     @Override
